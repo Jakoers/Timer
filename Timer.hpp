8cc034b4b4a90da9c950accb5c1a5bd3107aca16
Jako <Jako_S@outlook.com>
2025-06-27 22:19:20 +0800

    [init[

---
+++ Timer.hpp
@@ -0,0 +1,199 @@
+#ifndef TIMER_ACF53343_4EFA_45E1_9E22_6603F5CCAE69
+#define TIMER_ACF53343_4EFA_45E1_9E22_6603F5CCAE69
+
+#include <chrono>
+#include <cassert>
+#include <utility>
+#include <type_traits>
+#include <functional>
+#include <ostream>
+#include <ratio>
+
+template <typename T>
+struct is_ratio: std::false_type
+{
+};
+
+template <int64_t... Ns>
+struct is_ratio<std::ratio<Ns...>>: std::true_type
+{
+};
+
+template <typename T>
+constexpr bool is_ratio_v = is_ratio<T>::value;
+
+template <typename T>
+concept IsRatio = is_ratio_v<T>;
+
+/**
+ * @brief 通用高精度计时器，支持 start/stop 累积计时、peek 查询当前时间、measure
+ * 执行函数计时等功能
+ *
+ */
+class Timer
+{
+    template <IsRatio TargetUnit = std::chrono::seconds::period>
+    friend std::ostream &operator<<(std::ostream &os, Timer const &timer)
+    {
+        return os << timer.elapsedTime<TargetUnit>();
+    }
+
+public:
+#ifndef NDEBUG
+    Timer() :
+    elapsed(std::chrono::duration<double>::zero()), epoch(), isrunning(false)
+    {
+    }
+#else
+    Timer() : elapsed(std::chrono::duration<double>::zero()), epoch() {}
+#endif
+
+    /**
+     * @brief 重置计时器，清空累积时间并设置为未计时状态
+     *
+     */
+    void reset()
+    {
+        elapsed = {};
+        epoch   = {};
+#ifndef NDEBUG
+        isrunning = false;
+#endif
+    }
+
+    /**
+     * @brief 开始计时（不清空累计时间）
+     *
+     */
+    void start() noexcept
+    {
+#ifndef NDEBUG
+        assert(!isrunning && "Timer is already running.");
+        isrunning = true;
+#endif
+        epoch = st_clock::now();
+    }
+
+    /**
+     * @brief 停止计时并更新累计时间
+     *
+     */
+    void stop() noexcept
+    {
+#ifndef NDEBUG
+        assert(isrunning && "Timer has not been started.");
+        isrunning = false;
+#endif
+        elapsed += st_clock::now() - epoch;
+        // epoch    = {};
+    }
+
+    /**
+     * @brief 获取累计时间
+     *
+     * @tparam TargetUnit 单位比例，默认为 std::chrono::seconds::period
+     */
+    template <IsRatio TargetUnit = std::chrono::seconds::period>
+    double elapsedTime() const
+    {
+        return std::chrono::duration_cast<
+            std::chrono::duration<double, TargetUnit>>(elapsed)
+            .count();
+    }
+
+    /**
+     * @brief 获取当前（包括尚未 stop 的正在进行部分）累计的计时
+     *
+     * @tparam TargetUnit 单位比例 默认为 std::chrono::seconds::period
+     */
+    template <IsRatio TargetUnit = std::chrono::seconds::period>
+    double peek() const
+    {
+        auto nowElapsed = elapsed;
+#ifndef NDEBUG
+        if (isrunning)
+#endif
+            nowElapsed += st_clock::now() - epoch;
+        return std::chrono::duration_cast<
+            std::chrono::duration<double, TargetUnit>>(nowElapsed)
+            .count();
+    }
+
+    /**
+     * @brief 记录函数执行时间，并返回函数返回值
+     *
+     * @tparam Func 被调用的函数类型
+     * @tparam Args 函数参数类型
+     * @param func 被调用的函数
+     * @param args 函数参数
+     * @return func的返回值
+     */
+    template <typename Func, typename... Args>
+    auto measure(Func &&func, Args &&...args)
+    {
+        if constexpr (std::is_void_v<std::invoke_result_t<Func, Args...>>)
+        {
+            start();
+            std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+            stop();
+            return;
+        }
+        else
+        {
+            start();
+            auto &&result = std::invoke(
+                std::forward<Func>(func), std::forward<Args>(args)...);
+            stop();
+            return std::forward<decltype(result)>(result);
+        }
+    }
+
+    friend class DeterTimer;
+
+private:
+    using st_clock = std::chrono::steady_clock;
+    std::chrono::duration<double>     elapsed;
+    std::chrono::time_point<st_clock> epoch;
+#ifndef NDEBUG
+    bool isrunning;
+#endif
+
+    void addTime(std::chrono::duration<double> time) { elapsed += time; }
+};
+
+/**
+ * @brief
+ * Timer的辅助类，用于作用域内的计时（进入构造函数开始计时，析构自动加入计时器）
+ *
+ */
+class DeterTimer
+{
+public:
+    [[nodiscard]] explicit DeterTimer(Timer &timer) :
+    timer(timer), isrunning(true), epoch(st_clock::now())
+    {
+    }
+
+    ~DeterTimer()
+    {
+        if (isrunning) timer.addTime(st_clock::now() - epoch);
+    }
+
+    /**
+     * @brief 提前手动结束本次作用域计时，防止析构时重复累加
+     *
+     */
+    void stop()
+    {
+        timer.addTime(st_clock::now() - epoch);
+        isrunning = false;
+    }
+
+private:
+    using st_clock = std::chrono::steady_clock;
+    Timer                            &timer;
+    bool                              isrunning;
+    std::chrono::time_point<st_clock> epoch;
+};
+
+#endif /* TIMER_ACF53343_4EFA_45E1_9E22_6603F5CCAE69 */
