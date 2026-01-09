560f4b0b9283aba3349aa2e5c0b86fab97d2c566
Jako <Jako_S@outlook.com>
2026-01-09 16:06:19 +0800

    [example] 更新示例代码

--- examples/group/group_example.hpp
+++ examples/group/group_example.hpp
@@ -7,16 +7,50 @@ struct Timer11: Timer::TimerNode
 {
 };
 
+template <>
+struct Timer::NodeName<Timer11>
+{
+    static constexpr std::string_view name = "11";
+};
+
 struct Timer12: Timer::TimerNode
 {
+};
+
+template <>
+struct Timer::NodeName<Timer12>
+{
+    static constexpr std::string_view name = "12";
 };
 
 struct Timer21: Timer::TimerNode
 {
 };
 
+template <>
+struct Timer::NodeName<Timer21>
+{
+    static constexpr std::string_view name = "21";
+};
+
 struct Timer22: Timer::TimerNode
 {
+};
+
+template <>
+struct Timer::NodeName<Timer22>
+{
+    static constexpr std::string_view name = "22";
+};
+
+struct Timer2: Timer::TimerNode
+{
+};
+
+template <>
+struct Timer::NodeName<Timer2>
+{
+    static constexpr std::string_view name = "No naming required";
 };
 
 struct Group1: Timer::GroupNode
@@ -29,6 +63,12 @@ struct Timer::GroupTraits<Group1>
     using Children = std::tuple<Timer11, Timer12>;
 };
 
+template <>
+struct Timer::NodeName<Group1>
+{
+    static constexpr std::string_view name = "G1";
+};
+
 struct Group2: Timer::GroupNode
 {
 };
@@ -37,6 +77,13 @@ template <>
 struct Timer::GroupTraits<Group2>
 {
     using Children = std::tuple<Timer21, Timer22>;
+    using Master   = Timer2;
+};
+
+template <>
+struct Timer::NodeName<Group2>
+{
+    static constexpr std::string_view name = "G2";
 };
 
 struct GroupAll: Timer::GroupNode
@@ -49,7 +96,14 @@ struct Timer::GroupTraits<GroupAll>
     using Children = std::tuple<Group1, Group2>;
 };
 
-using myTimers = Timer::TimerRegistry<Timer11, Timer12, Timer21, Timer22>;
+template <>
+struct Timer::NodeName<GroupAll>
+{
+    static constexpr std::string_view name = "summary";
+};
+
+using myTimers =
+    Timer::TimerRegistry<Timer11, Timer12, Timer21, Timer22, Timer2>;
 
 void run_func();
 
--- examples/group/group_example_func.cpp
+++ examples/group/group_example_func.cpp
@@ -12,6 +12,9 @@ void run_func()
 {
     myTimers::get<Timer11>().measure(f, 100);
     myTimers::get<Timer12>().measure(f, 100);
+    myTimers::get<Timer2>().start();
     myTimers::get<Timer21>().measure(f, 500);
     myTimers::get<Timer22>().measure(f, 500);
+    f(100);
+    myTimers::get<Timer2>().stop();
 }
